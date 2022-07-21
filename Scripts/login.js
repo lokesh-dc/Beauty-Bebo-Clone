7584567338673d12d67d859f0acdc2e221751304
Devender <yadav0014dev@gmail.com>
2022-07-21 14:38:47 +0530

    login form

---
+++ Scripts/login.js
@@ -0,0 +1,5 @@
+import navbar from "../Components/navbar.js"
+import footer from "../Components/footer.js"
+document.getElementById("navbar").innerHTML = navbar()
+document.getElementById("footer").innerHTML = footer()
+
--- Scripts/signup.js
+++ Scripts/signup.js
@@ -1,7 +1,7 @@
 import navbar from "../Components/navbar.js"
-
+import footer from "../Components/footer.js"
 document.getElementById("navbar").innerHTML = navbar()
-
+document.getElementById("footer").innerHTML = footer()
 
 
 
